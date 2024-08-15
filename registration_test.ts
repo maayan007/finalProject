c54ecf0e281bb55dea578c4988d356ef1ce22310
Maayan Helfenbaum <84495832+maayan007@users.noreply.github.com>
2024-08-15 16:28:33 +0300

    Update registration_test.ts

--- registration_test.ts
+++ registration_test.ts
@@ -1,5 +1,5 @@
 import {Email, invalidEmails, Pages, testerLocators} from "./poms";
-import {uniqueEmail} from "./common";
+import {fillRegistrationForm, uniqueEmail} from "./common";
 
 Feature('User Registration');
 
@@ -16,35 +16,14 @@ Scenario('Successful registration', async ({ I }) => {
 });
 
 Scenario('Registration with existing email', async ({ I }) => {
-    I.amOnPage(Pages.Registration);
-    I.fillField(testerLocators.registration.firstName,"Maayan")
-    I.fillField(testerLocators.registration.lastName,"Testing")
-    I.fillField('Email', Email.ValidaRegistered);
-    I.fillField('Password', 'password123');
-    I.click(testerLocators.general.submitButton);
+    await fillRegistrationForm("Maayan", "Testing", Email.ValidaRegistered, "password123");
     I.seeElement(locate(testerLocators.registration.error).withText('Email address is already in use'));
-});
-
-Scenario('Invalid email and password', ({ I }) => {
-    I.amOnPage(Pages.Registration);
-    I.fillField(testerLocators.registration.firstName,"Maayan")
-    I.fillField(testerLocators.registration.lastName,"Testing")
-    I.fillField('Email', uniqueEmail);
-    I.fillField('Password', '123');
-    I.click(testerLocators.general.submitButton);
-    I.seeElement(locate(testerLocators.registration.error).withText
-    ('User validation failed: password: Path `password` (`123`) is shorter than the minimum allowed length (7).'));
 });
 
 Scenario('Email must contain exactly one "@" and not be at the start or end', ({ I }) => {
 
-    invalidEmails.forEach(email => {
-        I.amOnPage(Pages.Registration);
-        I.fillField(testerLocators.registration.firstName, "Maayan");
-        I.fillField(testerLocators.registration.lastName, "Testing");
-        I.fillField(testerLocators.registration.email, email);
-        I.fillField(testerLocators.registration.password, 'password123');
-        I.click(testerLocators.general.submitButton);
+    invalidEmails.forEach(async email => {
+        await fillRegistrationForm("Maayan", "Testing", email, "password123");
         I.seeElement(locate(testerLocators.registration.error).withText('User validation failed: email: Email is invalid'));
     });
 });
